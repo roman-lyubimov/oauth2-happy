66f357ead0b63a11457042d598f2dce40b53dc3e
Roman Lyubimov <raman.liubimau@gmail.com>
2018-07-09 16:17:16 +0300

    Initial release

---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var _ = require('../dist');
+
+describe('test', () => {
+  it('should to be a true', () => {
+    expect(true).to.be.true;
+  });
+});
